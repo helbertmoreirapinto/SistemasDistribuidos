7c22e0fc5a0ec2c77039f2516894889ed8694cde
Helbert M Pinto <helbert.tec.info@gmail.com>
2020-12-18 16:36:57 -0300

    add port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const middleware = require('./src/middleware/middleware')
-const port = 3333
+const port = process.env || 3333
 
 require('./database/index')
 
+app.use(cors())
 app.use(express.json())
 app.use(middleware)
 

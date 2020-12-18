c25f3ee22cdd9ce64b9506993321ddc20d61d863
Helbert M Pinto <helbert.tec.info@gmail.com>
2020-12-18 14:37:52 -0300

    config banco heroku

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  host: 'localhost',
-  database: 'sd_votacao_app',
-  username: 'postgres',
-  password: 'postgres',
+  host: 'ec2-54-160-133-106.compute-1.amazonaws.com',
+  database: 'df7rdv2khvp857',
+  username: 'gzofxwmmfqovuh',
+  password: '37924cb9f7df52ff1e73eaa2502f334c9bad7746aa149c59149c35e968f48ff1',
   port: 5432,
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    },
+    keepAlive: true
+  },
+  ssl: true
 }
-
